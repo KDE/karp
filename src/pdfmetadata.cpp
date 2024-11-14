f15c1cadc4493874660f7df8263cb965014a2231
Tomasz Bojczuk <seelook@gmail.com>
2024-11-14 16:56:23 +0100

    Dedicated class to deal with PDF metadata

---
+++ src/pdfmetadata.cpp
@@ -0,0 +1,195 @@
+// SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+// SPDX-FileCopyrightText: 2024 by Tomasz Bojczuk <seelook@gmail.com>
+
+#include "pdfmetadata.h"
+#include "version-deafed.h"
+
+using namespace Qt::Literals::StringLiterals;
+
+QByteArrayList PdfMetaData::m_tags = QByteArrayList() << QByteArrayLiteral("Title") << QByteArrayLiteral("Subject") << QByteArrayLiteral("Author")
+                                                      << QByteArrayLiteral("Keyword") << QByteArrayLiteral("Creator") << QByteArrayLiteral("Producer")
+                                                      << QByteArrayLiteral("CreationDate") << QByteArrayLiteral("ModDate");
+
+PdfMetaData::PdfMetaData()
+{
+    m_creator = QLatin1String("Deaf Ed ") + QLatin1String("(http://deafed.kde.org)");
+    m_producer = QLatin1String("Deaf Ed ") + QLatin1String(DEAFED_VERSION_STRING);
+    m_modDate = QDateTime::currentDateTime();
+    m_creationDate = m_modDate;
+}
+
+bool PdfMetaData::modyfied() const
+{
+    return m_modified;
+}
+
+QString PdfMetaData::title() const
+{
+    return m_title;
+}
+
+void PdfMetaData::setTitle(const QString &title)
+{
+    m_modified = true;
+    m_title = title;
+}
+
+QString PdfMetaData::subject() const
+{
+    return m_subject;
+}
+
+void PdfMetaData::setSubject(const QString &subject)
+{
+    m_modified = true;
+    m_subject = subject;
+}
+
+QString PdfMetaData::author() const
+{
+    return m_author;
+}
+
+void PdfMetaData::setAuthor(const QString &author)
+{
+    m_modified = true;
+    m_author = author;
+}
+
+QString PdfMetaData::creator() const
+{
+    return m_creator;
+}
+
+void PdfMetaData::setCreator(const QString &creator)
+{
+    m_modified = true;
+    m_creator = creator;
+}
+
+QString PdfMetaData::producer() const
+{
+    return m_producer;
+}
+
+void PdfMetaData::setProducer(const QString &producer)
+{
+    m_modified = true;
+    m_producer = producer;
+}
+
+QDateTime PdfMetaData::creationDate() const
+{
+    return m_creationDate;
+}
+
+void PdfMetaData::setCreationDate(const QDateTime &creationDate)
+{
+    m_modified = true;
+    m_creationDate = creationDate;
+}
+
+QDateTime PdfMetaData::modDate() const
+{
+    return m_modDate;
+}
+
+void PdfMetaData::setModDate(const QDateTime &modDate)
+{
+    m_modified = true;
+    m_modDate = modDate;
+}
+
+QString PdfMetaData::keyword() const
+{
+    return m_keyword;
+}
+
+void PdfMetaData::setKeyword(const QString &keyword)
+{
+    m_modified = true;
+    m_keyword = keyword;
+}
+
+/**
+ * kli18n("Title"), kli18n("Subject"), kli18n("Author"), kli18n("Keyword"), kli18n("Producer"), kli18n("Creator"),
+ * kli18n("Creation Date"), kli18n("Modification Date")
+ */
+QStringList PdfMetaData::model() const
+{
+    QStringList m;
+    m << m_title << m_subject << m_author << m_keyword << m_producer << m_creator << m_creationDate.toString(u"yyyy.MM.dd hh:mm:ss"_s)
+      << m_modDate.toString(u"yyyy.MM.dd hh:mm:ss"_s);
+    return m;
+}
+
+void PdfMetaData::setData(const QStringList &mData)
+{
+    int dataCount = qMin(PDF_METADATA_TAGS_COUNT, mData.size());
+    if (dataCount > 0)
+        m_title = mData[0];
+    if (dataCount > 1)
+        m_subject = mData[1];
+    if (dataCount > 2)
+        m_author = mData[2];
+    if (dataCount > 3)
+        m_keyword = mData[3];
+    if (dataCount > 4)
+        m_producer = mData[4];
+    if (dataCount > 5)
+        m_creator = mData[5];
+    // TODO what about dates?
+    // if (dataCount > 6)
+    //     m_creationDate = mData[6];
+    // if (dataCount > 7)
+    //     m_modDate = mData[7];
+    m_modified = true;
+}
+
+QByteArrayList PdfMetaData::tags()
+{
+    return m_tags;
+}
+
+QJsonObject PdfMetaData::toJSON()
+{
+    QJsonObject tagsJSON;
+    if (!m_title.isEmpty())
+        tagsJSON.insert(jsonTag(QPdfDocument::MetaDataField::Title), QJsonValue::fromVariant(jsonString(m_title)));
+    if (!m_subject.isEmpty())
+        tagsJSON.insert(jsonTag(QPdfDocument::MetaDataField::Subject), QJsonValue::fromVariant(jsonString(m_subject)));
+    if (!m_author.isEmpty())
+        tagsJSON.insert(jsonTag(QPdfDocument::MetaDataField::Author), QJsonValue::fromVariant(jsonString(m_author)));
+    if (!m_keyword.isEmpty())
+        tagsJSON.insert(jsonTag(QPdfDocument::MetaDataField::Keywords), QJsonValue::fromVariant(jsonString(m_keyword)));
+    if (!m_producer.isEmpty())
+        tagsJSON.insert(jsonTag(QPdfDocument::MetaDataField::Producer), QJsonValue::fromVariant(jsonString(m_producer)));
+    if (!m_creator.isEmpty())
+        tagsJSON.insert(jsonTag(QPdfDocument::MetaDataField::Creator), QJsonValue::fromVariant(jsonString(m_creator)));
+    // TODO: deal with time zone (+00)
+    if (m_creationDate.isValid())
+        tagsJSON.insert(jsonTag(QPdfDocument::MetaDataField::CreationDate), QJsonValue::fromVariant(m_creationDate.toString(u"u:D:yyyyMMddhhmmss+00''00''"_s)));
+    if (m_modDate.isValid())
+        tagsJSON.insert(jsonTag(QPdfDocument::MetaDataField::ModificationDate), QJsonValue::fromVariant(m_modDate.toString(u"u:D:yyyyMMddhhmmss+00''00''"_s)));
+    QJsonObject valueJSON;
+    valueJSON.insert(u"value"_s, tagsJSON);
+    return valueJSON;
+}
+
+QByteArray PdfMetaData::tag(QPdfDocument::MetaDataField tagId)
+{
+    int id = static_cast<int>(tagId);
+    if (id < 0 || id >= PDF_METADATA_TAGS_COUNT)
+        return QByteArray();
+    return m_tags[id];
+}
+
+QLatin1String PdfMetaData::jsonTag(QPdfDocument::MetaDataField tagId)
+{
+    return QLatin1String("/" + tag(tagId));
+}
+
+QString PdfMetaData::jsonString(const QString &s)
+{
+    return QLatin1String("u:") + s;
+}
---
+++ src/pdfmetadata.h
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+// SPDX-FileCopyrightText: 2024 by Tomasz Bojczuk <seelook@gmail.com>
+
+#pragma once
+
+#include <QDateTime>
+#include <QJsonObject>
+#include <QPdfDocument>
+
+#define PDF_METADATA_TAGS_COUNT (8)
+
+/**
+ * @brief PdfMetaData class handles available info of PDF file.
+ */
+class PdfMetaData
+{
+public:
+    PdfMetaData();
+
+    bool modyfied() const;
+
+    QString title() const;
+    void setTitle(const QString &title);
+
+    QString subject() const;
+    void setSubject(const QString &subject);
+
+    QString author() const;
+    void setAuthor(const QString &author);
+
+    QString creator() const;
+    void setCreator(const QString &creator);
+
+    QString producer() const;
+    void setProducer(const QString &producer);
+
+    QString keyword() const;
+    void setKeyword(const QString &keyword);
+
+    QDateTime creationDate() const;
+    void setCreationDate(const QDateTime &creationDate);
+
+    QDateTime modDate() const;
+    void setModDate(const QDateTime &modDate);
+
+    QStringList model() const;
+    void setData(const QStringList &mData);
+
+    /**
+     * List of tags used in PDF metadata ordered according to
+     * @p QPdfDocument::MetaDataField:
+     * Title, Subject, Author, Keyword, Creator, Producer, CreationDate, ModDate
+     */
+    static QByteArrayList tags();
+
+    /**
+     * Returns JSON object with tags of all existing metadata
+     */
+    QJsonObject toJSON();
+
+    static QByteArray tag(QPdfDocument::MetaDataField tagId);
+
+    /**
+     * Returns Metadata JSON tag, i.e. '/Author'.
+     * It is @p tag() with prepend '/'
+     * Use @p QPdfDocument::MetaDataField enumerator
+     */
+    static QLatin1String jsonTag(QPdfDocument::MetaDataField tagId);
+
+    static QString jsonString(const QString &s);
+
+private:
+    static QByteArrayList m_tags;
+
+    QString m_title;
+    QString m_subject;
+    QString m_author;
+    QString m_creator;
+    QString m_producer;
+    QString m_keyword;
+    QDateTime m_creationDate;
+    QDateTime m_modDate;
+    bool m_modified = false;
+};
+
+// Implementation of getters and setters
