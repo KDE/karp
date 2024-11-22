916017fa84f29ce130ea76b6b1bbc30b8a82ce8e
Luigi Toscano <luigi.toscano@tiscali.it>
2024-11-22 22:13:47 +0100

    dd a script to extract the translations
    Enable the extraction of the translatable messages
    in the application code.

---
+++ src/Messages.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: 2024 Luigi Toscano <luigi.toscano@tiscali.it>
+#
+# extract in a temporarily dummy cpp file
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >>rc.cpp
+# main code files
+$XGETTEXT `find . -name \*.cpp -o -name \*.h -o -name \*.qml` -o $podir/karp.pot
+# cleanup
+rm -f rc.cpp
