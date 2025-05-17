41cf6f299f11bf235fbf1ce88a54ee3567ebb3e3
Philipp Kiemle <philipp.kiemle@gmail.com>
2025-05-18 00:25:17 +0200

    Refine UI strings to align with KDE HIG

--- src/main.cpp
+++ src/main.cpp
@@ -116,7 +116,7 @@ int main(int argc, char *argv[])
         // The license this code is released under.
         KAboutLicense::GPL,
         // Copyright Statement.
-        i18n("(c) 2024-2025 The Karp Developers"));
+        i18n("© 2024-2025 The Karp Developers"));
     aboutData.addAuthor(i18nc("@info:credit", "Tomasz Bojczuk"), QString(), u"seelook@gmail.com"_s, u"https://sourceforge.net/u/seelook/profile"_s);
     aboutData.setTranslator(i18nc("NAME OF TRANSLATORS", "Your names"), i18nc("EMAIL OF TRANSLATORS", "Your emails"));
     KAboutData::setApplicationData(aboutData);
